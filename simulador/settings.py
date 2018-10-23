67f24151daa28c380f4d29d19f6be418b8cdddc1
Horacio Soto <hito_el11@hotmail.com>
2018-10-23 19:50:30 -0300

    Estructura inicial: layout y home.

--- simulador/settings.py
+++ simulador/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_VERSION = '20181008'
